0b4ad4b9ed05e7d3ce53e0fde52879aa9766fbac
Roman Pikulenko <roman.pikulenko@gmail.com>
2024-03-08 14:15:55 +0300

    Added materials table with paginations

--- angular-admin/src/app/app.config.ts
+++ angular-admin/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { initializeApp } from './app.initializer'
+import { initializeApp } from './app.initializer';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
@@ -10,6 +11,6 @@ export const appConfig: ApplicationConfig = {
     provide: APP_INITIALIZER,
     useFactory: initializeApp,
     multi: true,
-  },
+  }, provideAnimationsAsync(),
   ]
 };
--- angular-admin/src/app/secure/users/users.component.ts
+++ angular-admin/src/app/secure/users/users.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { User } from '../../interfaces/user';
 import { CommonModule } from '@angular/common';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent implements OnInit {
-  users: User[] = []
+export class UsersComponent implements OnInit, AfterViewInit {
+  columns = ['id', 'name', 'email', 'actions']
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator) paginator!: MatPaginator
 
   constructor(private userService: UsersService) { }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
   ngOnInit(): void {
     this.userService.all().then(e => {
-      if (e.success) this.users = e.users!
+      if (e.success) this.dataSource.data = e.users!
       else console.log(e)
     })
   }
