ca9f076dd02e8a8a8bbeb5e9dd41ccc6d98b8274
Roman Pikulenko <roman.pikulenko@gmail.com>
2024-03-17 17:37:34 +0300

    Added orderServing and order submitting

--- anguler-checkout/src/app/form/form.component.ts
+++ anguler-checkout/src/app/form/form.component.ts
@@ -5,11 +5,13 @@ import { Link } from '../interfaces/link';
 import { CommonModule } from '@angular/common';
 import { Product } from '../interfaces/product';
 import { User } from '../interfaces/user';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { OrderService } from '../services/order.service';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
@@ -18,10 +20,22 @@ export class FormComponent implements OnInit {
   link?: Link
   products: Product[] = []
   user?: User
+  quantities: Record<number, number> = {}
+  form!: FormGroup
 
-  constructor(private linkService: LinkService, private route: ActivatedRoute) { }
+  constructor(private linkService: LinkService, private orderService: OrderService, private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      first_name: '',
+      last_name: '',
+      email: '',
+      address: '',
+      country: '',
+      city: '',
+      zip: ''
+    })
+
     this.code = this.route.snapshot.params['code']
     this.linkService.get(this.code).then(e => {
       if (e.success) {
@@ -30,6 +44,28 @@ export class FormComponent implements OnInit {
         this.user = this.link!.user
       }
       else console.log(e)
+    })
+  }
+
+  total() {
+    const sum = this.products.filter(e => e.id in this.quantities).reduce((s, p) => s + p.price * this.quantities[p.id], 0)
+
+    return sum
+  }
+
+  submit() {
+    const data = {
+      code: this.code,
+      ...this.form.getRawValue(),
+      products: this.products.filter(e => e.id in this.quantities && this.quantities[e.id] > 0).map(p => ({
+        product_id: p.id,
+        quantity: this.quantities[p.id]
+      }
+      ))
+    }
+
+    this.orderService.create(data).then(e => {
+      console.log(e)
     })
   }
 }
---
+++ anguler-checkout/src/app/services/order.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { LinkResponse } from '../interfaces/service-responses/link-response';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { Link } from '../interfaces/link';
+import { environment } from '../../environments/environment.development';
+import { FetchModels } from './FetchModels';
+import { ServiceBaseResponse } from '../interfaces/service-responses/service-base-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor() { }
+
+  async create(data: any) {
+    const response = await axios.post(`${environment.api}/orders/`, data, {
+      withCredentials: true,
+      validateStatus: () => true
+    })
+
+    const handled = await this.handleResponse(response);
+
+    return handled
+  }
+
+  async handleResponse(response: AxiosResponse): Promise<ServiceBaseResponse> {
+
+    const result: ServiceBaseResponse = {
+      responseBody: response.data,
+      success: response.status >= 200 && response.status < 300
+    }
+
+    return result
+  }
+
+}
